65fa9b574437e19e0c7323f7dbeac09f67578b41
Rice <112427358+ricefarmernft@users.noreply.github.com>
2022-12-06 16:43:48 -0600

    Finished main logic for noTransfers.js

--- src/components/NoTransfers.js
+++ src/components/NoTransfers.js
@@ -7,27 +7,51 @@ import ApesMain from "./subcomponents/ApesMain";
 import SortMain from "./subcomponents/SortMain";
 import Web3 from "web3";
 
-const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/weAIDXHKw7995TqqNVUtFtLATPvXpYhz');
+const web3 = new Web3(
+  "https://eth-mainnet.g.alchemy.com/v2/weAIDXHKw7995TqqNVUtFtLATPvXpYhz"
+);
 
 const NoTransfers = () => {
+  const [untransferredApes, setUntransferredApes] = useState();
 
-    const options = {
+  useEffect(() => {
+    const array = [];
+    for (let i = 1036; i < 1050; i++) {
+      array[i] = i;
+    }
+
+    array.forEach((ape) => {
+      const apeIdBeginning =
+        "0x000000000000000000000000000000000000000000000000000000000000";
+
+      const hexId = web3.utils.numberToHex(ape).substring(2);
+      const fullHexId = hexId.padStart(4, "0");
+
+      const apeId = apeIdBeginning.concat(fullHexId);
+
+      const options = {
         fromBlock: 0, // Start search from the earliest block
-        toBlock: 'latest', // End search at the latest block
+        toBlock: "latest", // End search at the latest block
         address: "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d", // The address of the NFT contract
-        topics: ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-        null,
-        null,
-        "0x0000000000000000000000000000000000000000000000000000000000000000"
-    ] // The event signature for the Transfer event
+        topics: [
+          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+          null,
+          null,
+          `${apeId}`,
+        ],
       };
-    
-      web3.eth.getPastLogs(options)
-    .then(data => console.log(data));
 
-  return (
-    <div>NoTransfers</div>
-  )
-}
+      const logs = web3.eth.getPastLogs(options).then((data) => {
+        if (data.length <= 1) {
+          setUntransferredApes(ape);
+        }
+      });
+    });
+  }, []);
 
-export default NoTransfers
+  console.log(untransferredApes);
+
+  return <div>NoTransfers</div>;
+};
+
+export default NoTransfers;
