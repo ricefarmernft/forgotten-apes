770bc3428813b43ef5b899731912fa77d10351aa
Rice <112427358+ricefarmernft@users.noreply.github.com>
2022-12-15 16:05:43 -0600

    added ENV variables

--- src/App.js
+++ src/App.js
@@ -18,8 +18,6 @@ import {
 const { Content } = Layout;
 
 function App() {
-  console.log(process.env.REACT_APP_ALCHEMY_API_KEY);
-  console.log(process.env.REACT_APP_ETHERSCAN_API_KEY);
   return (
     <>
       <Layout>
