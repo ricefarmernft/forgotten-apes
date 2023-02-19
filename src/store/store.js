5aa483b03c704fcc83914d205aa0a921f482c270
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-02-19 13:29:09 -0600

    added lost apes and no transfers count

--- src/store/store.js
+++ src/store/store.js
@@ -18,8 +18,28 @@ const darkSlice = createSlice({
     },
   },
 });
+const darkSelector = (state) => state.darkSlice.dark;
 
-const darkSelector = (state) => state.darkSlice.dark
+
+const lostApesCountSlice = createSlice({
+  name: "lostApesCount",
+  initialState: 0,
+  reducers: {
+    setLostApesCount: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+const noTransfersCountSlice = createSlice({
+  name: "noTransfersCountSlice",
+  initialState: 0,
+  reducers: {
+    setNoTransfersCount: (state, action) => {
+      return action.payload;
+    },
+  },
+});
 
 
 const store = configureStore({
@@ -27,18 +47,18 @@ const store = configureStore({
     [etherscanApi.reducerPath]: etherscanApi.reducer,
     [alchemyApi.reducerPath]: alchemyApi.reducer,
     darkSlice: darkSlice.reducer,
+    lostApesCountSlice: lostApesCountSlice.reducer,
+    noTransfersCountSlice: noTransfersCountSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat([
-      etherscanApi.middleware,
-      alchemyApi.middleware,
-    ]),
+    }).concat([etherscanApi.middleware, alchemyApi.middleware]),
 });
 
 const { toggleDark } = darkSlice.actions;
-
+const { setLostApesCount } = lostApesCountSlice.actions;
+const { setNoTransfersCount } = noTransfersCountSlice.actions;
 
 export { store, toggleDark, darkSelector };
-
+export { setLostApesCount, setNoTransfersCount };
