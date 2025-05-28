fbcaaaf8b4b35aef91503317e54300d2f40e98ee
Rice <112427358+ricefarmernft@users.noreply.github.com>
2025-05-28 13:16:32 +0700

    updated etherscan API

--- src/services/etherscanApi.js
+++ src/services/etherscanApi.js
@@ -28,7 +28,8 @@ const sewerDeployer = "0xba5a9e9cbce12c70224446c24c111132becf9f1d";
 const sewerLog0 =
   "0x8ef78ce8289511059382ff6f0d889e212f440485eec5db7712c287745636ab85";
 
-const baseUrl = `https://api.etherscan.io/api`;
+// ✅ Fixed base URL (no query params)
+const baseUrl = `https://api.etherscan.io/v2/api`;
 
 const createRequest = (url) => ({ url });
 
@@ -39,37 +40,37 @@ export const etherscanApi = createApi({
     getApecoinApe: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${apecoinDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${apeLog0}&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${apecoinDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${apeLog0}&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
     getDogApe: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${dogDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${dogLog0}&topic1=${dogLog1}&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${dogDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${dogLog0}&topic1=${dogLog1}&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
     getOthersideApe: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${othersideDeployer}&fromBlock=14828755&toBlock=15356630&topic0=${othersideLog0}&topic1=${othersideLog1}&topic2=${yugaLabsAddress}&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${othersideDeployer}&fromBlock=14828755&toBlock=15356630&topic0=${othersideLog0}&topic1=${othersideLog1}&topic2=${yugaLabsAddress}&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
     getSewerApe: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000003&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000003&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
     getSewerApeBakc: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000004&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000004&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
     getSewerMutantBakc: builder.query({
       query: () =>
         createRequest(
-          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000002&page=1&offset=10000&apikey=${api}`
+          `?module=logs&action=getLogs&address=${sewerDeployer}&fromBlock=00000000&toBlock=99999999&topic0=${sewerLog0}&topic2=0x0000000000000000000000000000000000000000000000000000000000000002&page=1&offset=10000&chainid=1&apikey=${api}`
         ),
     }),
   }),
@@ -81,5 +82,5 @@ export const {
   useGetOthersideApeQuery,
   useGetSewerApeQuery,
   useGetSewerApeBakcQuery,
-  useGetSewerMutantBakcQuery
+  useGetSewerMutantBakcQuery,
 } = etherscanApi;
